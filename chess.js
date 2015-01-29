59d8df1964c3338d2a1a5624fb4f4c338de35bcc
David Albert <davidbalbert@gmail.com>
2015-01-28 21:55:43 -0500

    Board draws! Starting image preloading :).

--- chess.js
+++ chess.js
@@ -24,16 +24,6 @@
     }
   };
 
-  function Square(file, rank) {
-    this.file = file;
-    this.rank = rank;
-  }
-
-  Square.prototype = {
-    draw: function(screen) {
-    }
-  }
-
   function stepChar(c, step) {
     return String.fromCharCode(c.charCodeAt(0) + step);
   }
@@ -103,16 +93,115 @@
     return a.map(function (o) { return o[prop]; });
   }
 
+  function Pawn(color) {
+    this.color = color;
+  }
+
+  function Rook(color) {
+    this.color = color;
+  }
+
+  function Knight(color) {
+    this.color = color;
+  }
+
+  function Bishop(color) {
+    this.color = color;
+  }
+
+  function Queen(color) {
+    this.color = color;
+  }
+
+  function King(color) {
+    this.color = color;
+  }
+
+  var COLORS = {w: "white", b: "black"};
+  var PIECES = {
+    p: Pawn,
+    r: Rook,
+    k: Knight,
+    b: Bishop,
+    q: Queen,
+    K: King
+  };
+
+  function parsePiece(s) {
+    var parts = s.split("");
+
+    var color = COLORS[parts[0]];
+    assert(color, "color must be 'b' or 'w'");
+
+    var constructor = PIECES[parts[1]];
+    assert(constructor,  "'" + parts[1] + "' is not a valid piece");
+
+    return new constructor(color);
+  };
+
+  function Square(name, piece) {
+    this.name = name;
+    this.piece = piece;
+  }
+
+  Square.prototype = {
+    draw: function (screen, size) {
+      var pos = this.getPosition(size);
+
+      screen.fillStyle = this.getColor();
+      screen.fillRect(pos.x, pos.y, size, size);
+
+      if (this.piece) {
+        //this.piece.draw(screen, pos, side);
+      }
+    },
+
+    // Given the size of the square, return an object that represents the upper
+    // left corner of the square.
+    getPosition: function (size) {
+      return {
+        x: this.col() * size,
+        y: this.row() * size
+      };
+    },
+
+    getColor: function () {
+      var parts   = this.name.split("");
+      var rank    = parseInt(parts[1]);
+      var fileNum = parts[0].charCodeAt(0) - "a".charCodeAt(0) + 1; // a => 1, b => 2, etc.
+
+      if (this.row() % 2 === this.col() % 2) {
+        return "#ffffe0";
+      } else {
+        return "#556b2f";
+      }
+    },
+
+    // Zero indexed, starting from the top
+    row: function () {
+      var rank = parseInt(this.name.split("")[1]);
+
+      return 8 - rank;
+    },
+
+    // Zero indexed, starting from the left
+    col: function () {
+      var file = this.name.split("")[0];
+
+      return file.charCodeAt(0) - "a".charCodeAt(0);
+    }
+  };
+
   function parseBoardString(boardString) {
     var placements = boardString.split(" ").map(function (s) {
       if (s === "..") {
         return null;
       } else {
-        return Piece.fromString(s);
+        return parsePiece(s);
       }
     });
 
-    assert(placements.size === 64, "parseBoardString: boardString must have 64 squares");
+    assert(placements.length === 64, "parseBoardString: boardString must have 64 squares");
 
     var ranks = stringRange("8", "1", -1);
     var files = stringRange("a", "h", 1);
@@ -125,23 +214,28 @@
       var name = a[0];
       var piece = a[1];
 
-      return new Space(name, piece);
+      return new Square(name, piece);
     });
   }
 
   function Board(boardString, width, height) {
-    this.width  = width;
-    this.height = height;
+    assert(width === height, "board width must equal height");
+    assert(width % 8 == 0, "board size must be a multiple of 8");
+
+    this.side = width;
 
     this.squares = parseBoardString(boardString);
-    console.log(this.squares);
   }
 
   Board.prototype = {
     draw: function (screen) {
       for (var square of this.squares) {
-        //square.draw(screen);
+        square.draw(screen, this.sideLength());
       }
+    },
+
+    sideLength: function () {
+      return this.side / 8;
     },
 
     pieces: function () {
@@ -149,6 +243,23 @@
     }
   };
 
-  var game = new Game(document.getElementById("chess-canvas"));
-  game.redraw();
+  var IMAGE_NAMES = [
+    "images/black_bishop.png",
+    "images/black_king.png",
+    "images/black_knight.png",
+    "images/black_pawn.png",
+    "images/black_queen.png",
+    "images/black_rook.png",
+    "images/white_bishop.png",
+    "images/white_king.png",
+    "images/white_knight.png",
+    "images/white_pawn.png",
+    "images/white_queen.png",
+    "images/white_rook.png"
+  ];
+
+  loadImages(IMAGE_NAMES, function (images) {
+    var game = new Game(document.getElementById("chess-canvas"), images);
+    game.redraw();
+  });
 })();
